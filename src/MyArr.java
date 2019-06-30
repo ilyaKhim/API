5aadb52aab74b4dd4cf75ed2aaf829109129029c
ilyaKhim <ilya.himchenko@yandex.ru>
2019-06-30 15:02:57 +0300

    2 task done

--- src/MyArr.java
+++ src/MyArr.java
@@ -1,9 +1,9 @@
-import java.util.ArrayList;
+
 import java.util.Arrays;
 
 
 public class MyArr {
-    public int[] checkAndGet(int[] arr){
+    public int[] checkAndGetFour(int[] arr){
 
             for (int i = arr.length-1, count = 0; i>=0; i--, count++) {
                 if(arr[i] == 4){
@@ -13,15 +13,20 @@ public class MyArr {
                 }
             }
             throw new RuntimeException("there is no FOUR");
+    }
 
-
+    public boolean checkForOneAndFour(int[] arr){
+        for(int i = 0; i<arr.length; i++){
+            if(i == 4 || i == 1) return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
         try{
             int[] temp = {2, 2, 1,4, 32, 5, 6, 7, 45, 56, 545, 45};
-            System.out.println(Arrays.toString(new MyArr().checkAndGet(temp)));
-
+            System.out.println(Arrays.toString(new MyArr().checkAndGetFour(temp)));
+            System.out.println(new MyArr().checkForOneAndFour(temp));
         } catch (RuntimeException e){
             System.out.println(e.getMessage());
             e.printStackTrace();
--- src/MyArrTest.java
+++ src/MyArrTest.java
@@ -10,12 +10,13 @@ public class MyArrTest {
     @Before
     public void init(){
         test = new MyArr();
+
     }
     @Test
     public void testEquals() {
         int[] testing = {1,2,3,4,5,6,7,8,9,0};
         int[] expected = {5,6,7,8,9,0};
-        Assert.assertArrayEquals(expected, test.checkAndGet(testing));
+        Assert.assertArrayEquals(expected, test.checkAndGetFour(testing));
     }
 
     @Test
@@ -23,19 +24,38 @@ public class MyArrTest {
         int[] testing = {1,2,3,5,6,7,8,0};
         int[] expected = {0};
 
-        Assert.assertArrayEquals( expected, test.checkAndGet(testing));
+        Assert.assertArrayEquals( expected, test.checkAndGetFour(testing));
     }
 
     @Test
     public void testNotNull(){
         int[] testing = {1,2,3,4};
-        Assert.assertNotNull(test.checkAndGet(testing));
+        Assert.assertNotNull(test.checkForOneAndFour(testing));
     }
 
     @Test
     public void testTheSame(){
         int[] testing = {10,9,8,7,6,5,4,3,2,1};
         int[] expected = {3,2,1};
-        Assert.assertSame(expected, test.checkAndGet(testing));
+        Assert.assertSame(expected, test.checkForOneAndFour(testing));
+    }
+
+    @Test
+    public void test1(){
+        int[] testing = {1,2,3,4,5,6,7,8};
+        Assert.assertTrue(test.checkForOneAndFour(testing));
+    }
+
+    @Test
+    public void test2(){
+        int[] testing = {1,2,3,4,5,6,7,8};
+        Assert.assertEquals(true, test.checkForOneAndFour(testing));
+    }
+
+    @Test
+    public void test3(){
+        int[] testing = {1,2,3,4,5,6,7,8};
+        Assert.assertNotSame(false,test.checkForOneAndFour(testing));
+//        Assert.fail("test");
     }
 }
